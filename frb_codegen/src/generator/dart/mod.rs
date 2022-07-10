0b57fb6a4bb46fd64fdb290133509f1591f4e554
Satsuki Akiba <anosatsuk124@gmail.com>
2022-07-10 11:33:34 +0000

    add prefix fp

--- frb_codegen/src/generator/dart/mod.rs
+++ frb_codegen/src/generator/dart/mod.rs
@@ -174,7 +174,7 @@ fn generate_common_header() -> DartBasicCode {
     DartBasicCode {
         import: "import 'dart:convert';
             import 'dart:typed_data';
-            import 'package:fpdart/fpdart.dart';"
+            import 'package:fpdart/fpdart.dart' as fp;"
             .to_string(),
         part: "".to_string(),
         body: "".to_string(),
